fd6de40671374d00498bfa9ab5f5fcfb33c96f55
javierteixido <javierteixido@gmail.com>
2024-09-27 17:55:47 +0200

    make private variables inside the class

--- GameController.py
+++ GameController.py
@@ -3,56 +3,66 @@ import random
 class GameController:
 
     def __init__(self):
-        self.player_hands = {
+        self.__player_hands = {
             1: [],
             2: []
         }
-        self.dealer_hands = {
+        self.__dealer_hands = {
             1: [],
             2: []
         }
-        self.cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K']
-        self.deck = {
-            "spades": self.cards[:],
-            "hearts": self.cards[:],
-            "diamonds": self.cards[:],
-            "clubs": self.cards[:]
+        self.__cards = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 'J', 'Q', 'K']
+        self.__deck = {
+            "spades": self.__cards[:],
+            "hearts": self.__cards[:],
+            "diamonds": self.__cards[:],
+            "clubs": self.__cards[:]
         }
-        self.first_round = True
+        self.__first_round = True
 
     def __get_card(self):
-        tmp = random.choice(list(self.deck.keys()))
-        obj = random.choice(self.deck[tmp])
-        self.deck[tmp].remove(obj)
+        tmp = random.choice(list(self.__deck.keys()))
+        obj = random.choice(self.__deck[tmp])
+        self.__deck[tmp].remove(obj)
         return obj
 
+    def __check_equal_cards(self, hand):
+        print(f"> {hand[0]} vs. {hand[1]}")
+        return hand[0]==hand[1]
 
     def start_game(self):
-        self.player_hands[1].append(self.__get_card())
-        self.player_hands[1].append(self.__get_card())
-        self.dealer_hands[1].append(self.__get_card())
-        self.dealer_hands[1].append(self.__get_card())
-
+        self.__player_hands[1].append(self.__get_card())
+        self.__player_hands[1].append(self.__get_card())
+        self.__dealer_hands[1].append(self.__get_card())
+        self.__dealer_hands[1].append(self.__get_card())
+        self.game_status()
+        self.make_round()
 
     def game_status(self):
         print(f" Table cards:")
 
         # print player hands
         print(f" Player: ")
-        for hand in self.player_hands.keys():
-            print(self.player_hands[hand])
+        for hand in self.__player_hands.keys():
+            print(self.__player_hands[hand])
 
         # print dealer hands
         print(f" Dealer: ")
-        for hand in self.dealer_hands.keys():
-            print(self.dealer_hands[hand])
+        for hand in self.__dealer_hands.keys():
+            print(self.__dealer_hands[hand])
         print("")
 
         # print remaining deck
         print("Remaining cards:")
-        for brand in self.deck.keys():
-            print(f"{brand}: {self.deck[brand]}")
+        print(self.__deck)
 
+    def make_round(self):
+        if self.__first_round==True:
+            if self.__check_equal_cards(self.__player_hands[1]):
+                print(f"equal cards")
+            else:
+                print("no equal cards")
+        return
 
     def select_player_options(self):
         return None
--- welcome.py
+++ welcome.py
@@ -2,4 +2,3 @@ from GameController import GameController
 
 game = GameController()
 game.start_game()
-game.game_status()
